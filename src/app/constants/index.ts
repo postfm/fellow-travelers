a2d079edc4a6945c812fb13d01ee4041a228aa3e
postfm <72159645+postfm@users.noreply.github.com>
2024-09-14 13:06:56 +0300

    feat: Добавит названия стран

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,18 +1,18 @@
 export const Continents = [
   {
-    name: 'eroupe',
+    name: 'Europe',
     value: 'Европа',
   },
   {
-    name: 'asia',
+    name: 'Asia',
     value: 'Азия',
   },
   {
-    name: 'america',
+    name: 'America',
     value: 'Америка',
   },
   {
-    name: 'islands',
+    name: 'Islands',
     value: 'Острова',
   },
 ];
@@ -52,15 +52,19 @@ export const Countries = [
   },
   {
     name: 'Австрия',
-    place: '',
+    place: 'Europe',
   },
   {
     name: 'Азербайджан',
-    place: '',
+    place: 'Asia',
+  },
+  {
+    name: 'Акротири и Декелия',
+    place: 'Asia',
   },
   {
     name: 'Албания',
-    place: '',
+    place: 'Europe',
   },
   {
     name: 'Алжир',
@@ -72,64 +76,730 @@ export const Countries = [
   },
   {
     name: 'Андорра',
-    place: '',
+    place: 'Europe',
   },
   {
     name: 'Антигуа и Барбуда',
-    place: '',
+    place: 'Islands',
   },
   {
     name: 'Аргентина',
+    place: 'America',
+  },
+  {
+    name: 'Армения',
+    place: 'Asia',
+  },
+  {
+    name: 'Афганистан',
+    place: 'Asia',
+  },
+  {
+    name: 'Багамские Острова',
+    place: 'Islands',
+  },
+  {
+    name: 'Бангладеш',
+    place: 'Asia',
+  },
+  {
+    name: 'Барбадос',
+    place: 'Islands',
+  },
+  {
+    name: 'Бахрейн',
+    place: 'Asia',
+  },
+  {
+    name: 'Белоруссия',
+    place: 'Europe',
+  },
+  {
+    name: 'Белиз',
+    place: 'America',
+  },
+  {
+    name: 'Бельгия',
+    place: 'Europe',
+  },
+  {
+    name: 'Бенин',
     place: '',
   },
   {
-    name: '',
+    name: 'Болгария',
+    place: 'Europe',
+  },
+  {
+    name: 'Боливия',
+    place: 'America',
+  },
+  {
+    name: 'Босния и Герцеговина',
+    place: 'Europe',
+  },
+  {
+    name: 'Ботсвана',
     place: '',
   },
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  '',
-  'Армения',
-  'Афганистан',
-  'Багамские Острова',
-  'Бангладеш',
-  'Барбадос',
-  'Бахрейн',
-  'Белоруссия',
-  'Белиз',
-  'Бельгия',
-  'Бенин',
-  'Болгария',
-  'Боливия',
-  'Босния и Герцеговина',
-  'Ботсвана',
-  'Бразилия',
-  'Бруней',
-  'Буркина-Фасо',
-  'Бурунди',
-  'Бутан',
-  'Вануату',
-  'Великобритания',
-  'Венгрия',
-  'Венесуэла',
-  'Восточный Тимор',
-  'Вьетнам',
-  'Габон',
-  'Гаити',
-  'Гайана',
-  'Гамбия',
-  'Гана',
-  'Гватемала',
-  'Гвинея',
-  'Гвинея-Бисау',
-  'Германия',
-  'Гондурас',
-  'Гренада',
-  'Греция',
-  'Грузия',
+  {
+    name: 'Бразилия',
+    place: 'America',
+  },
+  {
+    name: 'Бруней',
+    place: 'Islands',
+  },
+  {
+    name: 'Буркина-Фасо',
+    place: '',
+  },
+  {
+    name: 'Бурунди',
+    place: '',
+  },
+  {
+    name: 'Бутан',
+    place: 'Asia',
+  },
+  {
+    name: 'Вануату',
+    place: 'Islands',
+  },
+  {
+    name: 'Ватикан',
+    place: 'Europe',
+  },
+  {
+    name: 'Великобритания',
+    place: 'Europe',
+  },
+  {
+    name: 'Венгрия',
+    place: 'Europe',
+  },
+  {
+    name: 'Венесуэла',
+    place: 'America',
+  },
+  {
+    name: 'Восточный Тимор',
+    place: 'Islands',
+  },
+  {
+    name: 'Вьетнам',
+    place: 'Asia',
+  },
+  {
+    name: 'Габон',
+    place: '',
+  },
+  {
+    name: 'Гаити',
+    place: 'Islands',
+  },
+  {
+    name: 'Гайана',
+    place: 'America',
+  },
+  {
+    name: 'Гамбия',
+    place: '',
+  },
+  {
+    name: 'Гана',
+    place: '',
+  },
+  {
+    name: 'Гватемала',
+    place: 'America',
+  },
+  {
+    name: 'Гвинея',
+    place: '',
+  },
+  {
+    name: 'Гвинея-Бисау',
+    place: '',
+  },
+  {
+    name: 'Германия',
+    place: 'Europe',
+  },
+  {
+    name: 'Гондурас',
+    place: 'America',
+  },
+  {
+    name: 'Гренада',
+    place: 'Islands',
+  },
+  {
+    name: 'Греция',
+    place: 'Europe',
+  },
+  {
+    name: 'Грузия',
+    place: 'Asia',
+  },
+  {
+    name: 'Дания',
+    place: 'Europe',
+  },
+  {
+    name: 'Джибути',
+    place: '',
+  },
+  {
+    name: 'Доминика',
+    place: 'Islands',
+  },
+  {
+    name: 'Доминикана',
+    place: 'Islands',
+  },
+  {
+    name: 'Египет',
+    place: 'Asia',
+  },
+  {
+    name: 'Замбия',
+    place: '',
+  },
+  {
+    name: 'Зимбабве',
+    place: '',
+  },
+  {
+    name: 'Израиль',
+    place: 'Asia',
+  },
+  {
+    name: 'Индия',
+    place: 'Asia',
+  },
+  {
+    name: 'Индонезия',
+    place: 'Asia',
+  },
+  {
+    name: 'Иордания',
+    place: 'Asia',
+  },
+  {
+    name: 'Ирак',
+    place: 'Asia',
+  },
+  {
+    name: 'Иран',
+    place: 'Asia',
+  },
+  {
+    name: 'Ирландия',
+    place: 'Europe',
+  },
+  {
+    name: 'Исландия',
+    place: 'Europe',
+  },
+  {
+    name: 'Испания',
+    place: 'Europe',
+  },
+  {
+    name: 'Италия',
+    place: 'Europe',
+  },
+  {
+    name: 'Кабо-Верде',
+    place: 'Islands',
+  },
+  {
+    name: 'Казахстан',
+    place: 'Asia',
+  },
+  {
+    name: 'Камбоджа',
+    place: 'Asia',
+  },
+  {
+    name: 'Камерун',
+    place: '',
+  },
+  {
+    name: 'Канада',
+    place: 'America',
+  },
+  {
+    name: 'Катар',
+    place: 'Asia',
+  },
+  {
+    name: 'Кения',
+    place: '',
+  },
+  {
+    name: 'Кипр',
+    place: 'Islands',
+  },
+  {
+    name: 'Кыргыстан',
+    place: 'Asia',
+  },
+  {
+    name: 'Кирибати',
+    place: 'Islands',
+  },
+  {
+    name: 'Китай',
+    place: 'Asia',
+  },
+  {
+    name: 'КНДР',
+    place: 'Asia',
+  },
+  {
+    name: 'Колумбия',
+    place: 'America',
+  },
+  {
+    name: 'Коморы',
+    place: 'Islands',
+  },
+  {
+    name: 'Коста-Рика',
+    place: 'America',
+  },
+  {
+    name: 'Куба',
+    place: 'Islands',
+  },
+  {
+    name: 'Кувейт',
+    place: 'Asia',
+  },
+  {
+    name: 'Лаос',
+    place: 'Asia',
+  },
+  {
+    name: 'Латвия',
+    place: 'Europe',
+  },
+  {
+    name: 'Лесото',
+    place: '',
+  },
+  {
+    name: 'Либерия',
+    place: '',
+  },
+  {
+    name: 'Ливан',
+    place: 'Asia',
+  },
+  {
+    name: 'Ливия',
+    place: '',
+  },
+  {
+    name: 'Литва',
+    place: 'Europe',
+  },
+  {
+    name: 'Лихтенштейн',
+    place: 'Europe',
+  },
+  {
+    name: 'Люксембург',
+    place: 'Europe',
+  },
+  {
+    name: 'Маврикий',
+    place: 'Islands',
+  },
+  {
+    name: 'Мавритания',
+    place: '',
+  },
+  {
+    name: 'Мадагаскар',
+    place: 'Islands',
+  },
+  {
+    name: 'Малави',
+    place: '',
+  },
+  {
+    name: 'Малайзия',
+    place: 'Asia',
+  },
+  {
+    name: 'Мали',
+    place: '',
+  },
+  {
+    name: 'Мальдивы',
+    place: 'Islands',
+  },
+  {
+    name: 'Мальта',
+    place: 'Europe',
+  },
+  {
+    name: 'Марокко',
+    place: '',
+  },
+  {
+    name: 'Маршалловы Острова',
+    place: 'Islands',
+  },
+  {
+    name: 'Мексика',
+    place: 'America',
+  },
+  {
+    name: 'Молдавия',
+    place: 'Europe',
+  },
+  {
+    name: 'Монако',
+    place: 'Europe',
+  },
+  {
+    name: 'Монголия',
+    place: 'Asia',
+  },
+  {
+    name: 'Мьянма',
+    place: 'Asia',
+  },
+  {
+    name: 'Намибия',
+    place: '',
+  },
+  {
+    name: 'Науру',
+    place: 'Islands',
+  },
+  {
+    name: 'Непал',
+    place: 'Asia',
+  },
+  {
+    name: 'Нигер',
+    place: '',
+  },
+  {
+    name: 'Нигерия',
+    place: '',
+  },
+  {
+    name: 'Нидерланды',
+    place: 'Europe',
+  },
+  {
+    name: 'Никарагуа',
+    place: 'America',
+  },
+  {
+    name: 'Ниуэ',
+    place: 'Islands',
+  },
+  {
+    name: 'Новая Зеландия',
+    place: 'Islands',
+  },
+  {
+    name: 'Норвегия',
+    place: 'Europe',
+  },
+  {
+    name: 'ОАЭ',
+    place: 'Asia',
+  },
+  {
+    name: 'Оман',
+    place: 'Asia',
+  },
+  {
+    name: 'Острова Кука',
+    place: 'Islands',
+  },
+  {
+    name: 'Пакистан',
+    place: 'Asia',
+  },
+  {
+    name: 'Палау',
+    place: 'Islands',
+  },
+  {
+    name: 'Панама',
+    place: 'America',
+  },
+  {
+    name: 'Папуа-Новая Гвинея',
+    place: 'Islands',
+  },
+  {
+    name: 'Парагвай',
+    place: 'America',
+  },
+  {
+    name: 'Перу',
+    place: 'America',
+  },
+  {
+    name: 'Польша',
+    place: 'Europe',
+  },
+  {
+    name: 'Португалия',
+    place: 'Europe',
+  },
+  {
+    name: 'Руанда',
+    place: '',
+  },
+  {
+    name: 'Россия',
+    place: 'Europe',
+  },
+  {
+    name: 'Румыния',
+    place: 'Europe',
+  },
+  {
+    name: 'Сальвадор',
+    place: 'America',
+  },
+  {
+    name: 'Самоа',
+    place: 'Islands',
+  },
+  {
+    name: 'Сан-Марино',
+    place: 'Europe',
+  },
+  {
+    name: 'Сан-Томе и Принсипи',
+    place: 'Islands',
+  },
+  {
+    name: 'Саудовская Аравия',
+    place: 'Asia',
+  },
+  {
+    name: 'Северная Македония',
+    place: 'Europe',
+  },
+  {
+    name: 'Северный Кипр',
+    place: 'Europe',
+  },
+  {
+    name: 'Сейшелы',
+    place: 'Islands',
+  },
+  {
+    name: 'Сент-Винсент и Гренадины',
+    place: 'Islands',
+  },
+  {
+    name: 'Сент-Китс и Невис',
+    place: 'Islands',
+  },
+  {
+    name: 'Сент-Люсия',
+    place: 'Islands',
+  },
+  {
+    name: 'Сенегал',
+    place: '',
+  },
+  {
+    name: 'Сербия',
+    place: 'Europe',
+  },
+  {
+    name: 'Сингапур',
+    place: 'Islands',
+  },
+  {
+    name: 'Сирия',
+    place: 'Asia',
+  },
+  {
+    name: 'Словакия',
+    place: 'Europe',
+  },
+  {
+    name: 'Словения',
+    place: 'Europe',
+  },
+  {
+    name: 'Соломоновы Острова',
+    place: 'Islands',
+  },
+  {
+    name: 'США',
+    place: 'America',
+  },
+  {
+    name: 'Суринам',
+    place: 'America',
+  },
+  {
+    name: 'Таджикистан',
+    place: 'Asia',
+  },
+  {
+    name: 'Таиланд',
+    place: 'Asia',
+  },
+  {
+    name: 'Тайвань',
+    place: 'Islands',
+  },
+  {
+    name: 'Танзания',
+    place: '',
+  },
+  {
+    name: 'Того',
+    place: '',
+  },
+  {
+    name: 'Тонга',
+    place: 'Islands',
+  },
+  {
+    name: 'Тринидад и Тобаго',
+    place: 'Islands',
+  },
+  {
+    name: 'Тувалу',
+    place: 'Islands',
+  },
+  {
+    name: 'Тунис',
+    place: '',
+  },
+  {
+    name: 'Туркмения',
+    place: 'Asia',
+  },
+  {
+    name: 'Турция',
+    place: 'Asia',
+  },
+  {
+    name: 'Уганда',
+    place: '',
+  },
+  {
+    name: 'Узбекистан',
+    place: 'Asia',
+  },
+  {
+    name: 'Украина',
+    place: 'Europe',
+  },
+  {
+    name: 'Уругвай',
+    place: 'America',
+  },
+  {
+    name: 'Федеративные Штаты Микронезии',
+    place: 'Islands',
+  },
+  {
+    name: 'Фиджи',
+    place: 'Islands',
+  },
+  {
+    name: 'Филиппины',
+    place: 'Islands',
+  },
+  {
+    name: 'Финляндия',
+    place: 'Europe',
+  },
+  {
+    name: 'Франция',
+    place: 'Europe',
+  },
+  {
+    name: 'Хорватия',
+    place: 'Europe',
+  },
+  {
+    name: 'Чад',
+    place: '',
+  },
+  {
+    name: 'Черногория',
+    place: 'Europe',
+  },
+  {
+    name: 'Чехия',
+    place: 'Europe',
+  },
+  {
+    name: 'Чили',
+    place: 'America',
+  },
+  {
+    name: 'Швейцария',
+    place: 'Europe',
+  },
+  {
+    name: 'Швеция',
+    place: 'Europe',
+  },
+  {
+    name: 'Шри-Ланка',
+    place: 'Islands',
+  },
+  {
+    name: 'Эквадор',
+    place: 'America',
+  },
+  {
+    name: 'Эритрея',
+    place: '',
+  },
+  {
+    name: 'Эсватини',
+    place: '',
+  },
+  {
+    name: 'Эстония',
+    place: 'Europe',
+  },
+  {
+    name: 'Эфиопия',
+    place: '',
+  },
+  {
+    name: 'ЮАР',
+    place: '',
+  },
+  {
+    name: 'Южная Корея',
+    place: 'Asia',
+  },
+  {
+    name: 'Южный Судан',
+    place: '',
+  },
+  {
+    name: 'Ямайка',
+    place: 'Islands',
+  },
+  {
+    name: 'Япония',
+    place: 'Asia',
+  },
 ];
