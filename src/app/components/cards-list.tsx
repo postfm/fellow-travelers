fd1ab7a8852396e4a0a325e302974bc9720e01ce
postfm <72159645+postfm@users.noreply.github.com>
2024-10-04 09:03:23 +0300

    fix: Еще раз исправит ошибку гидратации

--- src/app/components/cards-list.tsx
+++ src/app/components/cards-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Filters from './filters';
 import { Button, IconButton, ThemeProvider } from '@material-tailwind/react';
 import { ThemeIconButton } from '../constants/theme-icon-button';
@@ -18,6 +18,11 @@ interface CardListProps {
 export default function CardsList({ countrySelected }: CardListProps) {
   const [active, setActive] = React.useState(1);
   const [items, setItems] = useState(4);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   const filteredCards = countrySelected
     ? cards.filter((card) => card.country.some((element) => element === countrySelected))
@@ -59,140 +64,142 @@ export default function CardsList({ countrySelected }: CardListProps) {
   };
 
   return (
-    <section
-      className='flex justify-between w-full'
-      suppressHydrationWarning
-    >
-      <div className='flex flex-col'>
-        {filteredCards.map((card, index) => (
-          <CardNoSSR
-            key={index}
-            card={card}
-            hidden={index <= 3 ? '' : 'hidden'}
-          />
-        ))}
-        <Button
-          className='show-more mt-8 mb-[60px] mx-auto flex justify-between items-center font-bold text-xl leading-5 text-[#1D2E5B] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
-          variant={'text'}
-          ripple={false}
-          onClick={handleButtonClick}
-          placeholder={undefined}
-          onPointerEnterCapture={undefined}
-          onPointerLeaveCapture={undefined}
-        >
-          <svg
-            className='mr-6'
-            width='26'
-            height='26'
-            viewBox='0 0 26 26'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
+    isClient && (
+      <section
+        className='flex justify-between w-full'
+        suppressHydrationWarning
+      >
+        <div className='flex flex-col'>
+          {filteredCards.map((card, index) => (
+            <CardNoSSR
+              key={index}
+              card={card}
+              hidden={index <= 3 ? '' : 'hidden'}
+            />
+          ))}
+          <Button
+            className='show-more mt-8 mb-[60px] mx-auto flex justify-between items-center font-bold text-xl leading-5 text-[#1D2E5B] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
+            variant={'text'}
+            ripple={false}
+            onClick={handleButtonClick}
+            placeholder={undefined}
+            onPointerEnterCapture={undefined}
+            onPointerLeaveCapture={undefined}
           >
-            <rect
-              y='12'
+            <svg
+              className='mr-6'
               width='26'
-              height='2'
-              rx='1'
-              fill='#1D2E5B'
-            />
-            <rect
-              x='12'
-              y='26'
-              width='26'
-              height='2'
-              rx='1'
-              transform='rotate(-90 12 26)'
-              fill='#1D2E5B'
-            />
-          </svg>
-          Показать еще
-        </Button>
-        <ThemeProvider value={ThemeIconButton}>
-          <div className='flex justify-between items-center gap-4 w-full h-[100px] bg-white rounded-[20px] pl-[30px] pr-[40px]'>
-            <div className='flex items-center gap-2'>
-              <IconButton
-                {...getItemProps(1)}
-                ripple={false}
-                size={'lg'}
-                // className='hover:bg-transparent hover:opacity-60 active:bg-transparent'
-              >
-                1
-              </IconButton>
-              <IconButton
-                {...getItemProps(2)}
-                ripple={false}
-                size={'lg'}
-              >
-                2
-              </IconButton>
-              <IconButton
-                {...getItemProps(3)}
-                ripple={false}
-                size={'lg'}
-              >
-                3
-              </IconButton>
-              <IconButton
-                {...getItemProps(4)}
-                ripple={false}
-                size={'lg'}
-              >
-                4
-              </IconButton>
-            </div>
-            <div className='flex'>
-              <Button
-                variant='text'
-                className='flex items-center gap-2 rounded-full p-[15px] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
-                ripple={false}
-                onClick={prev}
-                disabled={active === 1}
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                <svg
-                  width='13'
-                  height='20'
-                  viewBox='0 0 13 20'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+              height='26'
+              viewBox='0 0 26 26'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <rect
+                y='12'
+                width='26'
+                height='2'
+                rx='1'
+                fill='#1D2E5B'
+              />
+              <rect
+                x='12'
+                y='26'
+                width='26'
+                height='2'
+                rx='1'
+                transform='rotate(-90 12 26)'
+                fill='#1D2E5B'
+              />
+            </svg>
+            Показать еще
+          </Button>
+          <ThemeProvider value={ThemeIconButton}>
+            <div className='flex justify-between items-center gap-4 w-full h-[100px] bg-white rounded-[20px] pl-[30px] pr-[40px]'>
+              <div className='flex items-center gap-2'>
+                <IconButton
+                  {...getItemProps(1)}
+                  ripple={false}
+                  size={'lg'}
+                  // className='hover:bg-transparent hover:opacity-60 active:bg-transparent'
                 >
-                  <path
-                    d='M0 10L9.75 1.33974L9.75 18.6603L0 10Z'
-                    fill='#1D2E5B'
-                  />
-                </svg>
-              </Button>
+                  1
+                </IconButton>
+                <IconButton
+                  {...getItemProps(2)}
+                  ripple={false}
+                  size={'lg'}
+                >
+                  2
+                </IconButton>
+                <IconButton
+                  {...getItemProps(3)}
+                  ripple={false}
+                  size={'lg'}
+                >
+                  3
+                </IconButton>
+                <IconButton
+                  {...getItemProps(4)}
+                  ripple={false}
+                  size={'lg'}
+                >
+                  4
+                </IconButton>
+              </div>
+              <div className='flex'>
+                <Button
+                  variant='text'
+                  className='flex items-center gap-2 rounded-full p-[15px] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
+                  ripple={false}
+                  onClick={prev}
+                  disabled={active === 1}
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
+                >
+                  <svg
+                    width='13'
+                    height='20'
+                    viewBox='0 0 13 20'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M0 10L9.75 1.33974L9.75 18.6603L0 10Z'
+                      fill='#1D2E5B'
+                    />
+                  </svg>
+                </Button>
 
-              <Button
-                variant='text'
-                className='flex items-center gap-2 rounded-full p-[15px] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
-                ripple={false}
-                onClick={next}
-                disabled={active === 4}
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-              >
-                <svg
-                  width='13'
-                  height='20'
-                  viewBox='0 0 13 20'
-                  fill='none'
-                  xmlns='http://www.w3.org/2000/svg'
+                <Button
+                  variant='text'
+                  className='flex items-center gap-2 rounded-full p-[15px] hover:bg-transparent active:bg-transparent hover:opacity-60 active:opacity-30'
+                  ripple={false}
+                  onClick={next}
+                  disabled={active === 4}
+                  placeholder={undefined}
+                  onPointerEnterCapture={undefined}
+                  onPointerLeaveCapture={undefined}
                 >
-                  <path
-                    d='M13 10L3.25 18.6603L3.25 1.33975L13 10Z'
-                    fill='#1D2E5B'
-                  />
-                </svg>
-              </Button>
+                  <svg
+                    width='13'
+                    height='20'
+                    viewBox='0 0 13 20'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path
+                      d='M13 10L3.25 18.6603L3.25 1.33975L13 10Z'
+                      fill='#1D2E5B'
+                    />
+                  </svg>
+                </Button>
+              </div>
             </div>
-          </div>
-        </ThemeProvider>
-      </div>
-      <Filters />
-    </section>
+          </ThemeProvider>
+        </div>
+        <Filters />
+      </section>
+    )
   );
 }
